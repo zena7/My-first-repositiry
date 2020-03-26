e52e3c13d8c15579805922329fec7df6a6a7adba
Наталья Зернова <natalia_zernova@MacBook-Air-Natala.local>
2020-03-26 21:31:57 +0300

    Login-verification-(If)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./index.css";
+
+let login = prompt(`Кто Вы?`);
+if (login == `Admin`) {
+  let passwordS = prompt(`Введите пароль`);
+  if (passwordS == `Я главный`) {
+    alert(`Здравствуйте!`);
+  } else {
+    alert(`Я Вас не знаю! `);
+  }
+} else {
+  alert(`Я Вас не знаю! `);
+}
