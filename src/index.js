c9b7ac5716a349a833249e7b87a18f342201f527
Наталья Зернова <natalia_zernova@sorochkina-lapt.ihps.nnov.ru>
2020-03-26 10:53:45 +0300

    Function_prompt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import "./index.css";
-let officialNameJS = prompt("how change you'r mood?");
-if (officialNameJS == "have sex") {
-  alert("Yay!");
-} else {
-  alert(": (");
-}
+// import "./index.css";
+// let officialNameJS = prompt("how change you'r mood?");
+// if (officialNameJS == "have sex") {
+//   alert("Yay!");
+// } else {
+//   alert(": (");
+// }
+
+yourName = prompt("Как тебя зовут?");
+alert(`Тебя зовут ${yourName}.`);
