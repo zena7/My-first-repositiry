480ec42eaefc307f198d5fff7ca0b4a9187e8249
Наталья Зернова <natalia_zernova@MacBook-Air-Natala.local>
2020-03-31 17:34:23 +0300

    Using break and continue for stopping cicles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./index.css";
+
+// let i = 10;
+// while (i) {
+//   alert(i);
+//   i -= 1;
+// }
+
+// for (let i = 0; i < 10; i += 1) {
+//   alert(i);
+// }
+
+// Только нечетные выводить
+// for (let i = 0; i < 7; i += 1) {
+//   if (i % 2 == 0) continue;
+//   alert(i);
+// }
+
+// for (let i = 2; i < 7; i += 1) {
+//   if (i % 2 == 1) break;
+//   alert(i);
+// }
