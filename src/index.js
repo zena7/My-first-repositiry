6ae5f3564f4591af54d5b817c2627822bb15010c
Наталья Зернова <natalia_zernova@MacBook-Air-Natala.local>
2020-04-02 15:49:20 +0300

    endless cycle

--- src/index.js
+++ src/index.js
@@ -23,10 +23,33 @@ import "./index.css";
 
 // Метки
 
-outer: for (let i = 0; i < 3; i += 1) {
-  for (let j = 0; j < 3; j += 1) {
-    let input = prompt(`Введите координаты  ${i}, ${j}`);
-    if (!input) break outer;
-  }
+// outer: for (let i = 0; i < 3; i += 1) {
+//   for (let j = 0; j < 3; j += 1) {
+//     let input = prompt(`Введите координаты  ${i}, ${j}`);
+//     if (!input) break outer;
+//   }
+// }
+// alert(`Готово !`);
+
+//  Вывести четные числа
+// let i;
+// let number = i;
+// for (i = 1; i <= 10; i += 1) {
+//   if (i % 2 == 1) continue;
+//   alert(i);
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   alert(`number ${i}!`);
+//   i += 1;
+// }
+//
+
+// Повторять цикл пока ввод не верен (бесконечный цикл)
+
+while (true) {
+  let value = prompt("Введите число больше 100");
+
+  if (Number(value) >= 100) break;
 }
-alert(`Готово !`);
