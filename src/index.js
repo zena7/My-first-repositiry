1d8fa5ddf2f52a9c1e341163c7c0df44c238c6f0
Наталья Зернова <natalia_zernova@MacBook-Air-Natala.local>
2020-04-03 18:32:10 +0300

    from switch to if else

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./index.css";
+`
+switch (browser) {
+  case "Edge":
+    alert("You've got the Edge!");
+    break;
+
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    alert("Okay we support these browsers too");
+    break;
+
+  default:
+    alert("We hope that this page looks ok!");
+}`;
+
+let browser;
+
+if (browser === "Edge") {
+  alert("You've got the Edge!");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === `Safari` ||
+  browser === `Opera`
+) {
+  alert("Okay we support these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
